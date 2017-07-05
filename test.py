d4b51aac6537623f10d448a8b91e59298853d308
Manish Munikar <munikarmanish@gmail.com>
2017-07-05 23:11:39 +0545

    cf_cost seems correct

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 #!/bin/env python3
 
-import unittest
 import logging
+import sys
+import unittest
+
 import numpy as np
+from scipy.io import loadmat
 
 import utils
 
@@ -16,12 +19,38 @@ class RecommenderTest(unittest.TestCase):
 class CostFunctionTest(unittest.TestCase):
 
     def test_numerical_gradient(self):
-        x = np.random.random((3,3))
+        x = np.random.random((3, 3))
         grad = 2 * x
         numgrad = utils.numerical_grad(lambda x: np.sum(x**2), x)
         np.testing.assert_almost_equal(grad, numgrad)
 
+    def test_cf_cost(self):
+        # logging.info("Loading dataset...")
+        R = utils.load_from_file('data/R.bin')
+        Y = utils.load_from_file('data/Y.bin')
+
+        # logging.info("Loading pre-trained parameters...")
+        _ = loadmat('data/movie_params.mat')
+        X = _.get('X')
+        Theta = _.get('Theta')
+
+        # reduce dataset
+        num_users = 4
+        num_movies = 5
+        num_features = 3
+        X = X[:num_movies, :num_features]
+        Theta = Theta[:num_users, :num_features]
+        Y = Y[:num_movies, :num_users]
+        R = R[:num_movies, :num_users]
+
+        params = np.append(X.flatten(), Theta.flatten())
+        cost = utils.cf_cost(params=params, Y=Y, R=R, num_features=num_features, reg=0)[0]
+        # logging.info("Expected cost = 22.22")
+        # logging.info("Computed cost = {:.2f}".format(cost))
+        self.assertAlmostEqual(22.22, cost, places=2)
+
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(levelname)10s: %(message)s')
+    logging.basicConfig(stream=sys.stderr,
+                        format='[%(levelname)s]: %(message)s', level=logging.INFO)
     unittest.main()
--- utils.py
+++ utils.py
@@ -26,7 +26,7 @@ def numerical_grad(f, x, h=1e-8):
         x[index] = original - h
         f_low = f(x)
         x[index] = original
-        grad[index] = (f_high - f_low) / (2*h)
+        grad[index] = (f_high - f_low) / (2 * h)
         it.iternext()
     return grad
 
@@ -42,6 +42,24 @@ def cf_cost(params, Y, R, num_features=Recommender.DEFAULT_NUM_FEATURES,
     J = 0
     X_grad = np.zeros_like(X)
     Theta_grad = np.zeros_like(Theta)
+
+    J = (.5 * np.sum(((np.dot(Theta, X.T).T - Y) * R)**2) +
+         ((reg / 2) * np.sum(Theta**2)) +
+         ((reg / 2) * np.sum(X**2)))
+
+    for i in range(num_movies):
+        idx = np.where(R[i, :] == 1)[0]  # users who have rated movie i
+        temp_theta = Theta[idx, :]  # parameter vector for those users
+        temp_Y = Y[idx, :]  # ratings given to movie i
+        X_grad[i, :] = np.sum(np.dot(np.dot(temp_theta, X[i, :]) - temp_Y.T,
+                                     temp_theta) + reg * X[i, :], axis=0)
+
+    for j in range(num_users):
+        idx = np.where(R[:, j] == 1)[0]
+        temp_X = X[idx, :]
+        temp_Y = Y[idx, j]
+        Theta_grad[j, :] = np.sum(np.dot(np.dot(Theta[j], temp_X.T) -
+                                         temp_Y, temp_X) + reg * Theta[j], axis=0)
 
     grad = np.append(X_grad.flatten(), Theta_grad.flatten())
     return (J, grad)
